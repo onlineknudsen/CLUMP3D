10eed33ace5c63fe3b723a2fb06dfc17f174b331
SNTPG <masonliu314@gmail.com>
2019-08-01 15:28:55 -0700

    Added SubMKS et al.

--- preliminaries.py
+++ preliminaries.py
@@ -1,5 +1,34 @@
 # probably move this back to main later
 
+import math
+
+# SubMKS' table is as follows:TCMB = 2.728
+s = 1
+day = 86400
+year = 3.15576*10**7
+g = 10**-3
+gr = g
+cm = 10**-2
+km = 10**3
+pc = 3.08568*10**16
+kpc = 1000*pc
+Mpc = 1000*kpc
+Gpc = 1000*Mpc
+AU = 1.49598*10**11
+G = 6.673*10**-11
+c = 299792458
+kB = 1.38065*10**-23
+me = 9.10939*10**-31
+mu = 1.66054*10**-27
+mp = 1.67262*10**-27
+# MSun already there
+sigmaT = 6.65246*10**-29
+hPlankConstant = 6.62608*10**-34
+eV = 1.60218*10**-19
+keV = 1000*eV
+erg = 10**-7
+Jy = 10**-26
+
 # units that are referenced later on, probably pull them from astropy in the near future
 MKS = 299792458
 GMKS = 6.673*10**-11 #(*G/.SubMKS*);
@@ -36,4 +65,8 @@ omegaLambda0Planck = 1 - omegaM0Planck
 # {\[CapitalOmega]M0Tmp1 = 0.3, hTmp1 = 0.7};
 
 def EzLambda(z, omegaM0, omegaLambda0):
-    return sqrt((1+z)**3 * omegaM0 + (1+z)**2 * (1 - omegaM0- o megaLambda0) +omegaLambda0)
+    return math.sqrt((1+z)**3 * omegaM0 + (1+z)**2 * (1 - omegaM0 - omegaLambda0) + omegaLambda0)
+
+# dunno what this line does but we'll figure it out soon: DInter = Interpolation[Table[{z, DistLambda[0,z,omegaM0Tmp1,omegaLambda0Tmp1]},{z,0,5,0.02}]]
+
+DToCGSFac =
