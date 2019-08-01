11a414b9077c3527452239245819d15ee1a1ba82
SNTPG <masonliu314@gmail.com>
2019-08-01 14:37:22 -0700

    Added more preliminary constants

--- preliminaries.py
+++ preliminaries.py
@@ -1,9 +1,26 @@
 # probably move this back to main later
 
-# units that are referenced later on
+# units that are referenced later on, probably pull them from astropy in the near future
 MKS = 299792458
 GMKS = 6.673*10**-11 #(*G/.SubMKS*);
 MSunMKS = 1.989103*10**30 #(*MSun/.SubMKS*);
 pcMKS = 3.08567802*10**16 #(*Mpc/.SubMKS/.SubMKS*);
 kpcMKS = 10**3*pcMKS #(*Mpc/.SubMKS/.SubMKS*);
 MpcMKS = 10**6*pcMKS #(*Mpc/.SubMKS/.SubMKS*);
+omegaM0v = 1 - 0.7
+deltaOmegaM0v = 0.016
+omegaLambda0v = 0.7
+hv = 0.7
+deltahv = 0.014
+omegaM0XXL = 1 - 0.72
+hXXL = 0.70
+omegaM0WMAP9 = 0.28
+omegaLambda0WMAP9 = 0.72
+hWMAP9 = 0.70
+sigma8WMAP9 = 0.82
+omegaM0PSZ = 0.3
+hPSZ = 0.7
+omegaM0Planck = 0.3175
+sigma8Planck = 0.8344
+hPlanck = 0.671
+omegaLambda0Planck = 1 - omegaM0Planck
