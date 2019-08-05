85d0330272a033cedbf9f7043adc1b52e4fd4bea
SNTPG <masonliu314@gmail.com>
2019-08-04 23:07:19 -0700

    Added first FITS reader, added more config variables

--- cluster_config.py
+++ cluster_config.py
@@ -4,3 +4,13 @@ theta_ev = 0.0
 ra = 0.0
 dec = 0.0
 redshift = 0.0
+
+#these variable names are ugly right now, they come from MLE-E_WL.fits
+mean_gl_cdelt1 = 0.0
+mean_gl_cdelt2 = 0.0
+mean_gl_crval1 = 0.0
+mean_gl_crval2 = 0.0
+mean_gl_naxis1 = 0.0
+mean_gl_naxis2 = 0.0
+mean_gl_crpix1 = 0.0
+mean_gl_crpix2 = 0.0
--- file_readers.py
+++ file_readers.py
@@ -1,4 +1,6 @@
 import cluster_config
+import astropy
+from astropy.io import fits
 import os
 
 def import_cluster_info(working_dir):
@@ -36,3 +38,28 @@ def import_gl_info(working_dir):
                     theta_ev_sum += float(line)
                 i += 1
         cluster_config.theta_ev = theta_ev_sum / 2
+
+# trying to make FITS readers here
+
+def import_gl_mean_info(working_dir): # there might be a better name for this
+    with fits.open(os.path.join(working_dir, "GL", "gl_info.dat")) = hdul #saves it as an HDUList, will look up more documentation soon
+        #hdul.info() would give us some debugging opportunities here
+        header = hdul[0].header
+        cluster_config.mean_gl_cdelt1 = header["CDELT1"]
+        cluster_config.mean_gl_cdelt2 = header["CDELT2"]
+        cluster_config.mean_gl_crval1 = header["CRVAL1"]
+        cluster_config.mean_gl_crval2 = header["CRVAL2"]
+        cluster_config.mean_gl_naxis1 = header["NAXIS1"]
+        cluster_config.mean_gl_naxis2 = header["NAXIS2"]
+        cluster_config.mean_gl_crpix1 = header["CRPIX1"]
+        cluster_config.mean_gl_crpix2 = header["CRPIX2"]
+
+        # need to figure out what these lines do: 
+        '''kGridTmp1 = kTabFits[[2, 1]] // Chop;
+        
+        (*{0.,0} (physical coordinates) is the optical coordinates*)
+        DataGridTabWL2 = 
+            Partition[
+                Table[{({j, i}(*-CentralPixCoord*)), ({j, i} - {CRPIX1, CRPIX2})*{CDELT1,CDELT1} + {CRVAL1, CRVAL2}, kConvFac2*kGridTmp1[[i, j]], 
+       0.(*\[Delta]k*)} // Flatten, {i, 1, NAXIS2}, {j, 1, NAXIS1}] //
+     Flatten, 6];'''    
