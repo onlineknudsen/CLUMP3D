3dde09eaccc25b13d9cc2deb8c9f78efd655ec2d
SNTPG <masonliu314@gmail.com>
2019-08-04 23:32:42 -0700

    Fixed a lot of errors; I understand FITS and HDU

--- file_readers.py
+++ file_readers.py
@@ -1,5 +1,4 @@
 import cluster_config
-import astropy
 from astropy.io import fits
 import os
 
@@ -41,25 +40,26 @@ def import_gl_info(working_dir):
 
 # trying to make FITS readers here
 
-def import_gl_mean_info(working_dir): # there might be a better name for this
-    with fits.open(os.path.join(working_dir, "GL", "gl_info.dat")) = hdul #saves it as an HDUList, will look up more documentation soon
-        #hdul.info() would give us some debugging opportunities here
-        header = hdul[0].header
-        cluster_config.mean_gl_cdelt1 = header["CDELT1"]
-        cluster_config.mean_gl_cdelt2 = header["CDELT2"]
-        cluster_config.mean_gl_crval1 = header["CRVAL1"]
-        cluster_config.mean_gl_crval2 = header["CRVAL2"]
-        cluster_config.mean_gl_naxis1 = header["NAXIS1"]
-        cluster_config.mean_gl_naxis2 = header["NAXIS2"]
-        cluster_config.mean_gl_crpix1 = header["CRPIX1"]
-        cluster_config.mean_gl_crpix2 = header["CRPIX2"]
+def import_gl_mean_info(working_dir):
+    hdul = fits.open(os.path.join(working_dir, "GL", "U16", "MLE-E_WL.fits")) #saves it as an HDUList, will look up more documentation soon
+    #hdul.info() would give us some debugging opportunities here
+    head = hdul[0].header
+    cluster_config.mean_gl_cdelt1 = head["CDELT1"]
+    cluster_config.mean_gl_cdelt2 = head["CDELT2"]
+    cluster_config.mean_gl_crval1 = head["CRVAL1"]
+    cluster_config.mean_gl_crval2 = head["CRVAL2"]
+    cluster_config.mean_gl_naxis1 = head["NAXIS1"]
+    cluster_config.mean_gl_naxis2 = head["NAXIS2"]
+    cluster_config.mean_gl_crpix1 = head["CRPIX1"]
+    cluster_config.mean_gl_crpix2 = head["CRPIX2"]
 
-        # need to figure out what these lines do: 
-        '''kGridTmp1 = kTabFits[[2, 1]] // Chop;
+    # need to figure out what these lines do: 
+    '''kGridTmp1 = kTabFits[[2, 1]] // Chop;
         
         (*{0.,0} (physical coordinates) is the optical coordinates*)
         DataGridTabWL2 = 
             Partition[
                 Table[{({j, i}(*-CentralPixCoord*)), ({j, i} - {CRPIX1, CRPIX2})*{CDELT1,CDELT1} + {CRVAL1, CRVAL2}, kConvFac2*kGridTmp1[[i, j]], 
-       0.(*\[Delta]k*)} // Flatten, {i, 1, NAXIS2}, {j, 1, NAXIS1}] //
-     Flatten, 6];'''    
+       0.(*[Delta]k*)} // Flatten, {i, 1, NAXIS2}, {j, 1, NAXIS1}] //
+     Flatten, 6];'''
+    hdul.close()
